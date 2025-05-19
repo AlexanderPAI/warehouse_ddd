42436d83a0f199f5bd94b4138a68a06de07860bf
AlexanderPAI <lawlikeart@yandex.ru>
2025-05-19 11:57:29 +0300

    Postgres container

--- infrastructure/database.py
+++ infrastructure/database.py
@@ -1,5 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 
 DATABASE_URL = "sqlite:///warehouse.db"
 
